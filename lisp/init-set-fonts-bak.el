f05ce9a2c0f3392c7c25feac942575b335a615d5
Vincent4307 <3205027978@qq.com>
2016-03-07 21:23:19 +0800

    add font backup

--- lisp/init-set-fonts-bak.el
+++ lisp/init-set-fonts-bak.el
@@ -16,19 +16,19 @@
 ;
 ;
 ;;;;;;;;;;;;;;;;;for windows;;;;;;;;;;;;;;;;;;;;;;;;
-(custom-set-faces
+;(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-(set-face-attribute
- 'default nil :font "DejaVu Sans Mono 12")
+;(set-face-attribute
+; 'default nil :font "DejaVu Sans Mono 12")
 ; 'default nil :font "Droid Sans Mono 12")
 
- (dolist (charset '(kana han symbol cjk-misc bopomofo))
-  (set-fontset-font (frame-parameter nil 'font)
-            charset
-            (font-spec :family "Microsoft YaHei" :size 18))) )
+; (dolist (charset '(kana han symbol cjk-misc bopomofo))
+;  (set-fontset-font (frame-parameter nil 'font)
+;            charset
+;            (font-spec :family "Microsoft YaHei" :size 18))) )
 
 
 (provide 'init-set-fonts)
