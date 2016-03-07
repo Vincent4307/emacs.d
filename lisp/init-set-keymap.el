fab619f298ce64dccbbca30dfe2bc4d496e1742f
Vincent4307 <3205027978@qq.com>
2016-03-08 00:12:30 +0800

    set keymap for turn-on evil and turn-off evil

--- lisp/init-set-keymap.el
+++ lisp/init-set-keymap.el
@@ -2,4 +2,9 @@
 
 (define-key global-map [C-S-return] 'set-mark-command)
 
+;for evil mode
+(define-key global-map [C-return] 'turn-off-evil-mode)
+(define-key global-map [S-return] 'turn-on-evil-mode)
+;for evil mode
+
 (provide 'init-set-keymap)
